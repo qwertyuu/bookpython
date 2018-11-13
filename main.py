65f7fff8920ff417791b04ceae6b0b0d5b45f324
Raphaël Côté <raphael.cote@ellipse-synergie.com>
2018-11-13 15:37:45 -0500

    Add airtable and testing

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pydotenv
+from airtable import Airtable
+
 from bibli import Bibli
 
 
 def main():
     env = pydotenv.Environment(check_file_exists=True)
+    airtable = Airtable(env.get('AIRTABLE_BASE_KEY'), 'books', api_key=env.get('AIRTABLE_API_KEY'))
     b = Bibli(env.get('CARD_NUMBER'), env.get('PASSWORD'))
     b.hydrate_books()
     b.close()
