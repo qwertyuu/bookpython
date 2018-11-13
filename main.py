402a30ba92d5ec68789818e1ee35656db38a305b
Raphaël Côté <raphael.cote@ellipse-synergie.com>
2018-11-13 15:37:16 -0500

    Revert test code

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ def main():
     env = pydotenv.Environment(check_file_exists=True)
     b = Bibli(env.get('CARD_NUMBER'), env.get('PASSWORD'))
     b.hydrate_books()
-    input()
     b.close()
     pass
 
